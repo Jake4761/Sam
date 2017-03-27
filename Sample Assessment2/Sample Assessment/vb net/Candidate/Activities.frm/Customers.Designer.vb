b6d2f694dedc7cac6db17d8f8200d598dab043cf
Jake4761 <jakemifsud79@gmail.com>
2017-03-27 12:21:27 +0100

    Add files via upload

---
+++ Sample Assessment2/Sample Assessment/vb net/Candidate/Activities.frm/Customers.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Customers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnInput = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnInput
+        '
+        Me.btnInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInput.Location = New System.Drawing.Point(28, 357)
+        Me.btnInput.Name = "btnInput"
+        Me.btnInput.Size = New System.Drawing.Size(414, 85)
+        Me.btnInput.TabIndex = 0
+        Me.btnInput.Text = "INPUT"
+        Me.btnInput.UseVisualStyleBackColor = True
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(24, 21)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(59, 20)
+        Me.lblName.TabIndex = 1
+        Me.lblName.Text = "Name :"
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLastName.Location = New System.Drawing.Point(24, 70)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(94, 20)
+        Me.lblLastName.TabIndex = 2
+        Me.lblLastName.Text = "Last Name :"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEmail.Location = New System.Drawing.Point(24, 115)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(56, 20)
+        Me.lblEmail.TabIndex = 4
+        Me.lblEmail.Text = "Email :"
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(342, 18)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 26)
+        Me.txtName.TabIndex = 9
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLastName.Location = New System.Drawing.Point(342, 67)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(100, 26)
+        Me.txtLastName.TabIndex = 10
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEmail.Location = New System.Drawing.Point(342, 112)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(100, 26)
+        Me.txtEmail.TabIndex = 12
+        '
+        'Customers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 476)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.btnInput)
+        Me.Name = "Customers"
+        Me.Text = "Customers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnInput As System.Windows.Forms.Button
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents lblLastName As System.Windows.Forms.Label
+    Friend WithEvents lblEmail As System.Windows.Forms.Label
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+End Class
---
+++ Sample Assessment2/Sample Assessment/vb net/Candidate/Activities.frm/Customers.vb
@@ -0,0 +1,67 @@
+﻿Imports System.IO
+Public Class Customers
+
+    'Creating a structure with name, lastname and email. To be used when storing data in a text file
+
+    Private Structure CustomerData
+        Dim Name As String
+        Dim LastName As String
+        Dim Email As String
+    End Structure
+
+
+    Private Sub btnInput_Click(sender As System.Object, e As System.EventArgs) Handles btnInput.Click
+        Dim Name As String = txtName.Text
+        Dim LastName As String = txtLastName.Text
+        Dim Email As String = txtEmail.Text
+        'Above i am declaring and giving value to the persons details
+
+
+        ' This is simple validation using a function which is below, it checks if all the variables are not null or are too big 
+        ' The first value is the actual data and the second number is how long the string is allowed to be
+        If Validation(Name, 35) = False Or Validation(LastName, 35) = False Or Validation(Email, 255) = False Then
+            MsgBox(" Please Fill out all the fields.")
+        Else
+
+
+
+
+
+
+
+
+            'If it passes the validation i want to write it to a text box which is what happens here.
+
+            Dim CustomerDetails As New CustomerData
+
+            Dim sw As New StreamWriter(Application.StartupPath & "\Customer.txt", True)
+            CustomerDetails.Name = LSet(txtName.Text, 35)
+            CustomerDetails.LastName = LSet(txtLastName.Text, 35)
+            CustomerDetails.Email = LSet(txtEmail.Text, 255)                'Filling the structure with data.
+
+            sw.WriteLine(CustomerDetails.Name & CustomerDetails.LastName & CustomerDetails.Email)
+            sw.Close()
+
+
+
+
+        End If
+
+
+    End Sub
+
+    Private Function Validation(ByVal Data As String, Length As Integer)
+        If Data.Length > Length Then
+            Return False
+
+        ElseIf Data = "" Then
+            MsgBox("please ensure all fields are full")
+            Return False
+        Else
+            Return True
+        End If
+        MsgBox(Data)
+
+    End Function
+
+End Class
