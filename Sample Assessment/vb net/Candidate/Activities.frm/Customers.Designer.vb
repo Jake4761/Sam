9c6b9221bf5dbfe4ae485365a2d44cd4d5f391c6
Jake4761 <jakemifsud79@gmail.com>
2017-03-28 21:47:28 +0100

    Add files via upload

---
+++ Sample Assessment/vb net/Candidate/Activities.frm/Customers.Designer.vb
@@ -0,0 +1,256 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Customers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnInput = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.lblPhone = New System.Windows.Forms.Label()
+        Me.lblDoB = New System.Windows.Forms.Label()
+        Me.lblAddress = New System.Windows.Forms.Label()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.dtpDoB = New System.Windows.Forms.DateTimePicker()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtSession = New System.Windows.Forms.TextBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.txtInputSearch = New System.Windows.Forms.TextBox()
+        Me.lblSearchID = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnInput
+        '
+        Me.btnInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInput.Location = New System.Drawing.Point(28, 294)
+        Me.btnInput.Name = "btnInput"
+        Me.btnInput.Size = New System.Drawing.Size(514, 84)
+        Me.btnInput.TabIndex = 0
+        Me.btnInput.Text = "INPUT"
+        Me.btnInput.UseVisualStyleBackColor = True
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(24, 21)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(59, 20)
+        Me.lblName.TabIndex = 1
+        Me.lblName.Text = "Name :"
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLastName.Location = New System.Drawing.Point(24, 56)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(94, 20)
+        Me.lblLastName.TabIndex = 2
+        Me.lblLastName.Text = "Last Name :"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEmail.Location = New System.Drawing.Point(24, 93)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(56, 20)
+        Me.lblEmail.TabIndex = 4
+        Me.lblEmail.Text = "Email :"
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtFirstName.Location = New System.Drawing.Point(342, 18)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(200, 26)
+        Me.txtFirstName.TabIndex = 9
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLastName.Location = New System.Drawing.Point(342, 53)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(200, 26)
+        Me.txtLastName.TabIndex = 10
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEmail.Location = New System.Drawing.Point(342, 93)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(200, 26)
+        Me.txtEmail.TabIndex = 12
+        '
+        'lblPhone
+        '
+        Me.lblPhone.AutoSize = True
+        Me.lblPhone.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblPhone.Location = New System.Drawing.Point(26, 133)
+        Me.lblPhone.Name = "lblPhone"
+        Me.lblPhone.Size = New System.Drawing.Size(119, 20)
+        Me.lblPhone.TabIndex = 13
+        Me.lblPhone.Text = "Phone Number:"
+        '
+        'lblDoB
+        '
+        Me.lblDoB.AutoSize = True
+        Me.lblDoB.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblDoB.Location = New System.Drawing.Point(25, 169)
+        Me.lblDoB.Name = "lblDoB"
+        Me.lblDoB.Size = New System.Drawing.Size(106, 20)
+        Me.lblDoB.TabIndex = 14
+        Me.lblDoB.Text = "Date Of Birth:"
+        '
+        'lblAddress
+        '
+        Me.lblAddress.AutoSize = True
+        Me.lblAddress.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblAddress.Location = New System.Drawing.Point(26, 205)
+        Me.lblAddress.Name = "lblAddress"
+        Me.lblAddress.Size = New System.Drawing.Size(72, 20)
+        Me.lblAddress.TabIndex = 15
+        Me.lblAddress.Text = "Address:"
+        '
+        'lblID
+        '
+        Me.lblID.AutoSize = True
+        Me.lblID.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblID.Location = New System.Drawing.Point(25, 250)
+        Me.lblID.Name = "lblID"
+        Me.lblID.Size = New System.Drawing.Size(87, 20)
+        Me.lblID.TabIndex = 16
+        Me.lblID.Text = "Session ID"
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtPhone.Location = New System.Drawing.Point(342, 130)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(200, 26)
+        Me.txtPhone.TabIndex = 17
+        '
+        'dtpDoB
+        '
+        Me.dtpDoB.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.dtpDoB.Location = New System.Drawing.Point(342, 164)
+        Me.dtpDoB.Name = "dtpDoB"
+        Me.dtpDoB.Size = New System.Drawing.Size(200, 26)
+        Me.dtpDoB.TabIndex = 18
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtAddress.Location = New System.Drawing.Point(342, 202)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(200, 26)
+        Me.txtAddress.TabIndex = 19
+        '
+        'txtSession
+        '
+        Me.txtSession.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.txtSession.Location = New System.Drawing.Point(342, 247)
+        Me.txtSession.Name = "txtSession"
+        Me.txtSession.Size = New System.Drawing.Size(200, 26)
+        Me.txtSession.TabIndex = 20
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSearch.Location = New System.Drawing.Point(30, 384)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(514, 84)
+        Me.btnSearch.TabIndex = 21
+        Me.btnSearch.Text = "Search And Recieve"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'txtInputSearch
+        '
+        Me.txtInputSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtInputSearch.Location = New System.Drawing.Point(367, 492)
+        Me.txtInputSearch.Name = "txtInputSearch"
+        Me.txtInputSearch.Size = New System.Drawing.Size(133, 26)
+        Me.txtInputSearch.TabIndex = 22
+        '
+        'lblSearchID
+        '
+        Me.lblSearchID.AutoSize = True
+        Me.lblSearchID.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSearchID.Location = New System.Drawing.Point(26, 495)
+        Me.lblSearchID.Name = "lblSearchID"
+        Me.lblSearchID.Size = New System.Drawing.Size(312, 20)
+        Me.lblSearchID.TabIndex = 23
+        Me.lblSearchID.Text = "Please Enter The ID you want to search for"
+        '
+        'Customers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 551)
+        Me.Controls.Add(Me.lblSearchID)
+        Me.Controls.Add(Me.txtInputSearch)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.txtSession)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.dtpDoB)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.lblID)
+        Me.Controls.Add(Me.lblAddress)
+        Me.Controls.Add(Me.lblDoB)
+        Me.Controls.Add(Me.lblPhone)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.btnInput)
+        Me.Name = "Customers"
+        Me.Text = "Customers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnInput As System.Windows.Forms.Button
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents lblLastName As System.Windows.Forms.Label
+    Friend WithEvents lblEmail As System.Windows.Forms.Label
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+    Friend WithEvents lblPhone As System.Windows.Forms.Label
+    Friend WithEvents lblDoB As System.Windows.Forms.Label
+    Friend WithEvents lblAddress As System.Windows.Forms.Label
+    Friend WithEvents lblID As System.Windows.Forms.Label
+    Friend WithEvents txtPhone As System.Windows.Forms.TextBox
+    Friend WithEvents dtpDoB As System.Windows.Forms.DateTimePicker
+    Friend WithEvents txtAddress As System.Windows.Forms.TextBox
+    Friend WithEvents txtSession As System.Windows.Forms.TextBox
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents txtInputSearch As System.Windows.Forms.TextBox
+    Friend WithEvents lblSearchID As System.Windows.Forms.Label
+End Class
---
+++ Sample Assessment/vb net/Candidate/Activities.frm/Customers.vb
@@ -0,0 +1,177 @@
+﻿Imports System.IO
+Public Class Customers
+
+    'Creating a structure with name, lastname and email. To be used when storing data in a text file
+
+    Private Structure CustomerData
+        Dim FirstName As String
+        Dim LastName As String
+        Dim Email As String
+        Dim Address As String
+        Dim Phone As String
+        Dim DOB As String
+        Dim SessionID As Integer
+    End Structure
+
+
+    Private Sub btnInput_Click(sender As System.Object, e As System.EventArgs) Handles btnInput.Click
+
+        'Above i am declaring and giving value to the persons details
+
+        Dim FirstName As String = txtFirstName.Text
+        Dim LastName As String = txtLastName.Text
+        Dim Email As String = txtEmail.Text
+        Dim Address As String = txtAddress.Text
+        Dim Phone As String = txtPhone.Text
+        Dim DOB As String = dtpDoB.Text
+        Dim SessionID As Integer
+        SessionID = Val(txtSession.Text)
+
+
+        ' This is simple validation using a function which is below, it checks if all the variables are not null or are too big 
+        ' The first value is the actual data and the second number is how long the string is allowed to be
+        If LengthCheck(Name, 35) = False Or LengthCheck(LastName, 35) = False Or LengthCheck(Email, 255) = False Or LengthCheck(Address, 100) = False Or LengthCheck(Phone, 11) = False Or LengthCheck(SessionID, 4) = False Then
+            MsgBox(" Please Fill out all the fields.")
+        ElseIf DataTypeCheck(FirstName, False) = False Or DataTypeCheck(LastName, False) = False Or DataTypeCheck(Phone, True) = False Or DataTypeCheck(SessionID, True) = False Then
+            MsgBox("Please use the correct data types")
+
+        Else
+
+
+            Dim SessionCopy As Boolean = False
+            Dim Check() As String = File.ReadAllLines(Dir$("Customer.txt"))
+            For I = 0 To UBound(Check)
+
+                If Trim(Mid(Check(I), 1, 4)) = txtSession.Text Then SessionCopy = True
+
+
+            Next I
+
+            If SessionCopy = False Then
+
+
+                'If it passes the validation i want to write it to a text box which is what happens here.
+
+                Dim CustomerDetails As New CustomerData
+
+                Dim sw As New StreamWriter(Application.StartupPath & "\Customer.txt", True)
+                CustomerDetails.FirstName = LSet(txtFirstName.Text, 35)
+                CustomerDetails.LastName = LSet(txtLastName.Text, 35)
+                CustomerDetails.Email = LSet(txtEmail.Text, 255)                'Filling the structure with data.
+                CustomerDetails.Address = LSet(txtAddress.Text, 100)
+                CustomerDetails.Phone = LSet(txtPhone.Text, 11)
+                CustomerDetails.DOB = LSet(dtpDoB.Text, 20)
+                CustomerDetails.SessionID = LSet(Val(txtSession.Text), 4)
+                sw.WriteLine(CustomerDetails.SessionID & CustomerDetails.LastName & CustomerDetails.Email & CustomerDetails.Address & CustomerDetails.Phone & CustomerDetails.DOB & CustomerDetails.FirstName)
+                sw.Close()
+
+                MsgBox("Saved")
+
+            Else : MsgBox("Please choose a new session ID that one is taken")
+
+            End If
+
+
+        End If
+
+
+    End Sub
+
+    Private Function LengthCheck(ByVal Data As String, Length As Integer)
+
+        'This function has two inputs, the actual data and the length the data is allowed to be 
+        'If the length is too big or the data is null then False is returned otherwise True is returned
+        If Data.Length > Length Then
+            Return False
+
+        ElseIf Data = "" Then
+            MsgBox("please ensure all fields are full")
+            Return False
+        Else
+            Return True
+        End If
+
+
+    End Function
+
+    Private Function DataTypeCheck(ByVal Data As String, Type As Boolean)
+
+        'This function has two inputs, the actual data and the type of this dats in the form of boolean
+        'If the boolean is true then the data contains numbers and no letters
+        'If the value is false then it contains letters and no numbers
+        'isnumeric is used to check whether numebers are used and the corresponding true/false value returned
+
+        If Type = True Then
+            If IsNumeric(Data) = True Then
+                Return True
+
+            Else : Return False
+
+            End If
+
+
+
+        ElseIf Type = False Then
+
+            If IsNumeric(Data) = False Then
+
+                Return True
+
+            Else : Return False
+
+            End If
+
+        End If
+
+        Return False
+
+    End Function
+
+    Private Sub btnSearch_Click(sender As System.Object, e As System.EventArgs) Handles btnSearch.Click
+
+        'Here i am declaring and assigning the value of the search they want to search
+        Dim SearchValue As String = txtInputSearch.Text
+
+        If LengthCheck(SearchValue, 4) = False Or DataTypeCheck(SearchValue, True) = False Then
+            MsgBox("Please ensure the search value is a 4 digit Session ID")
+            'Above checks whether their search value fits what i allow using my functions
+        Else
+            Dim Found As Boolean
+            Found = False
+            'Found will be an identifyer 
+            'This is assigning the value of the text document to CustomerData
+            Dim Customerdata() As String = File.ReadAllLines(Dir$("Customer.txt"))
+            For I = 0 To UBound(Customerdata)
+                'This For loop will repeat for all the lines in the text file
+                If Trim(Mid(Customerdata(I), 1, 4)) = txtInputSearch.Text And Not txtInputSearch.Text = "" Then
+                    Found = True
+                End If
+
+                'The above if statement will find if this line in the text document contains the search value
+
+                If Found = True Then
+
+
+                    txtSession.Text = Trim(Mid(Customerdata(I), 1, 4))
+                    txtLastName.Text = Trim(Mid(Customerdata(I), 5, 35))
+                    txtEmail.Text = Trim(Mid(Customerdata(I), 40, 255))
+                    txtAddress.Text = Trim(Mid(Customerdata(I), 295, 100))
+                    txtPhone.Text = Trim(Mid(Customerdata(I), 395, 11))
+                    '  dtpDoB.Text = Trim(Mid(Customerdata(I), 406, 20))
+                    'Sadly DTP is bad and this doesnt work
+                    txtFirstName.Text = Trim(Mid(Customerdata(I), 426, 35))
+                    'These are all statements that assign the value in the text document line to the textboxes
+                    MsgBox("SearchFound")
+                    I = UBound(Customerdata)
+                    'Above stops the for loops
+                End If
+
+            Next I
+
+            If Found = False Then
+                MsgBox("Search not found please ensure the value is correct")
+            End If
+        End If
+
+    End Sub
+End Class
